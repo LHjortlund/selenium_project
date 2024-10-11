d729f4498900f1bb00119cb49e113ea7e8c18982
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-11 09:29:11 +0200

    selenium interaction

--- third.py
+++ third.py
@@ -1,17 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 
 #keep Chrome browser open after program finish
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-# chrome_options.add_argument("start-maximized")
 driver = webdriver.Chrome(options=chrome_options)
+driver.maximize_window()
 #navigate to the wished URL Page
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 #challenge print the statistic number in front page
-article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 # print(article_count.text)
 
 #XPATH to the solution
@@ -22,15 +26,29 @@ article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 # all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
 # all_portals.click()
 
-
-#interact with search bar - type a wished text in the search bar on the front page
+#Wait until the search bar is clickable
+# try:
+#     search_bar = WebDriverWait(driver, 10).until(
+#         EC.element_to_be_clickable((By.CSS_SELECTOR, "#p-search a"))
+#     )
+#     search_bar.click()
+#
+#     # Now interact with the search field
+#     search = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.NAME, "search"))
+#     )
+#     search.send_keys("Python", Keys.ENTER)
+# except TimeoutException:
+#     print("Element was not interactable within the given time")
+# finally:
+#
+#     driver.quit()
+# interact with search bar - type a wished text in the search bar on the front page
 search_bar = driver.find_element(By.CSS_SELECTOR, value="#p-search a")
 search_bar.click()
 
-#using search nar
+# using search nar
 search = driver.find_element(By.NAME, value="search")
-search.send_keys("Python")
-
+search.send_keys("Python", Keys.ENTER)
 
 # driver.quit()
-
